1721fc7b23979f1314952b98a71cd53492d289c5
ruiming <zhourui@streamax.com>
2021-09-10 11:22:11 +0800

    add some code for r50 clean test

--- configs/recognition/csn/fatigue_r50_clean.py
+++ configs/recognition/csn/fatigue_r50_clean.py
@@ -17,12 +17,12 @@ model = dict(
 )
 # dataset settings
 dataset_type = 'FatigueCleanDataset'
-data_root = '/default_group/fatigue_clips'
-data_root_val = '/default_group/fatigue_clips'
+data_root = '/zhourui/workspace/pro/fatigue/data/rawframes/new_clean/fatigue_clips'
+data_root_val = '/zhourui/workspace/pro/fatigue/data/rawframes/new_clean/fatigue_clips'
 facerect_data_prefix = '/zhourui/workspace/pro/fatigue/data/clean/fatigue_info_from_yolov5'
-ann_file_train = '/default_group/fatigue_anns/20210824_fatigue_pl_less_than_50_fatigue_full_info_all_path.json'
-ann_file_val = '/default_group/fatigue_anns/20210824_fatigue_pl_less_than_50_fatigue_full_info_all_path.json'
-ann_file_test = '/default_group/fatigue_anns/20210824_fatigue_pl_less_than_50_fatigue_full_info_all_path.json'
+ann_file_train = '/zhourui/workspace/pro/fatigue/data/clean/fatigue_anns/20210824_fatigue_pl_less_than_50_fatigue_full_info_all_path.json'
+ann_file_val = '/zhourui/workspace/pro/fatigue/data/clean/fatigue_anns/20210824_fatigue_pl_less_than_50_fatigue_full_info_all_path.json'
+ann_file_test = '/zhourui/workspace/pro/fatigue/data/clean/fatigue_anns/20210824_fatigue_pl_less_than_50_fatigue_full_info_all_path.json'
 test_save_results_path = 'work_dirs/fatigue_r50_clean/valid_results_testone.npy'
 test_save_label_path = 'work_dirs/fatigue_r50_clean/valid_label_testone.npy'
 
