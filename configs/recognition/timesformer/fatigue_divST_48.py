47c203bf9e07859371494d1327e55658f9ab30eb
ruiming <zhourui@streamax.com>
2021-09-11 12:06:51 +0800

    batch size to 1

--- configs/recognition/timesformer/fatigue_divST_48.py
+++ configs/recognition/timesformer/fatigue_divST_48.py
@@ -80,7 +80,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs', 'label'])
 ]
 data = dict(
-    videos_per_gpu=2,
+    videos_per_gpu=1,
     workers_per_gpu=4,
     pin_memory=False,
     train=dict(
