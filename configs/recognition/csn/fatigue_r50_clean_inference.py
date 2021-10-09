cbb8f9b61a96bca4f91c456bd5448a023bb19e20
ruiming <zhourui@streamax.com>
2021-10-09 17:00:46 +0800

    inference

--- configs/recognition/csn/fatigue_r50_clean_inference.py
+++ configs/recognition/csn/fatigue_r50_clean_inference.py
@@ -2,20 +2,43 @@ _base_ = [
     '../../_base_/models/ircsn_r152.py', '../../_base_/default_runtime.py'
 ]
 
+checkpoint_config = dict(interval=1)
+log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook'),
+    ])
+# runtime settings
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
 # model settings
 model = dict(
+    type='Recognizer3D',
     backbone=dict(
+        type='ResNet3dCSN',
+        pretrained2d=False,
+        pretrained=None,
         depth=50,
+        with_pool2=False,
+        bottleneck_mode='ir',
         norm_eval=True,
         bn_frozen=True,
-        pretrained=  # noqa: E251
-        'https://download.openmmlab.com/mmaction/recognition/csn/ircsn_from_scratch_r50_ig65m_20210617-ce545a37.pth'  # noqa: E501
-    ),
+        zero_init_residual=False),
     cls_head=dict(
-        num_classes=2
-    ),
-    test_cfg=dict(average_clips='org_prob')
-)
+        type='I3DHead',
+        num_classes=2,
+        in_channels=2048,
+        spatial_type='avg',
+        dropout_ratio=0.5,
+        init_std=0.01),
+    # model training and testing settings
+    train_cfg=None,
+    test_cfg=dict(average_clips='org_prob'))
 
 # dataset settings
 clip_len = 48
