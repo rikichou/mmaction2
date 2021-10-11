8196017d23062b28e8fb0056c7b8ca3c76b817e9
ruiming <zhourui@streamax.com>
2021-10-11 14:55:28 +0800

    add new config for fatigue_divST_96_int2_move.py

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -13,14 +13,14 @@ from .hvu_dataset import HVUDataset
 from .image_dataset import ImageDataset
 from .pose_dataset import PoseDataset
 from .rawframe_dataset import RawframeDataset
-from .fatigue_rawframe_dataset import FatigueRawframeDataset, FatigueCleanDataset, FatigueNormalDataset
+from .fatigue_rawframe_dataset import FatigueRawframeDataset, FatigueCleanDataset, FatigueNormalDataset, FatigueMoveDataset
 from .rawvideo_dataset import RawVideoDataset
 from .ssn_dataset import SSNDataset
 from .video_dataset import VideoDataset
 
 __all__ = [
     'VideoDataset', 'build_dataloader', 'build_dataset', 'RepeatDataset',
-    'RawframeDataset', 'FatigueRawframeDataset', 'FatigueCleanDataset', 'FatigueNormalDataset', 'BaseDataset', 'ActivityNetDataset', 'SSNDataset',
+    'RawframeDataset', 'FatigueRawframeDataset', 'FatigueCleanDataset', 'FatigueNormalDataset', 'FatigueMoveDataset', 'BaseDataset', 'ActivityNetDataset', 'SSNDataset',
     'HVUDataset', 'AudioDataset', 'AudioFeatureDataset', 'ImageDataset',
     'RawVideoDataset', 'AVADataset', 'AudioVisualDataset',
     'BaseMiniBatchBlending', 'CutmixBlending', 'MixupBlending', 'DATASETS',
